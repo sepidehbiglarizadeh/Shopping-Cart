1678ae5f44609c85ae2e16506ce82c0918f3a9d3
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-08-02 17:44:16 +0430

    first commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import styles from "./NavBar.module.css"
+
+const NavBar = () => {
+    return ( 
+        <header className={styles.header}>
+            <h1>My Shopping Cart</h1>
+        </header>
+     );
+}
+ 
+export default NavBar;

b1bf03d3b2b40d08d62ca5cf9ed035be597c29a6
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-08-06 16:42:17 +0430

    The number of products added to the NavBar component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,13 @@
+import { useProducts } from "../Provider/ProductsProvider";
 import styles from "./NavBar.module.css"
 
 const NavBar = () => {
+    const products= useProducts();
+    const totalItems= products.filter((p)=>p.quantity >0).length;
     return ( 
         <header className={styles.header}>
             <h1>My Shopping Cart</h1>
+            <span className={styles.count}>{totalItems}</span>
         </header>
      );
 }
--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -6,26 +6,44 @@ const ProductsList = () => {
   const products = useProducts();
   const dispatch = useProductsActions();
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <span className={styles.header_item}>Description</span>
-        <span>Quantity</span>
-        <span>Price</span>
+  const renderProducts = () => {
+    if (products.length === 0)
+      return (
+        <div className={styles.text}>there is no product in your cart</div>
+      );
+
+    return (
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <span className={styles.header_item}>Description</span>
+          <span>Quantity</span>
+          <span>Price</span>
+        </div>
+        {products.map((product) => {
+          return (
+            <Product
+              product={product}
+              key={product.id}
+              onIncrement={() =>
+                dispatch({ type: "increment", id: product.id })
+              }
+              onDecrement={() =>
+                dispatch({ type: "decrement", id: product.id })
+              }
+              onDelete={() => dispatch({ type: "delete", id: product.id })}
+            />
+          );
+        })}
       </div>
-      {products.map((product) => {
-        return (
-          <Product
-            product={product}
-            key={product.id}
-            onIncrement={() => dispatch({ type: "increment", id: product.id })}
-            onDecrement={() => dispatch({ type: "decrement", id: product.id })}
-            onDelete={() => dispatch({ type: "delete", id: product.id })}
-          />
-        );
-      })}
-    </div>
-  );
+    );
+  };
+
+  return ( 
+    <div>
+        {!products.length && <div className={styles.text}>go to shopping</div>}
+        {renderProducts()}
+      </div>
+   );
 };
 
 export default ProductsList;
